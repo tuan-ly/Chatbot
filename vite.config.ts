6d9acd3a7c25f1f61074a2dc24ecb92f4c5989d9
tuan-ly <89176508+tuan-ly@users.noreply.github.com>
2024-10-10 05:02:01 +0700

    edit vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+	return {
+		plugins: [sveltekit()],
+		define: {
+			'import.meta.env.VITE_SUPABASE_URL': JSON.stringify(env.VITE_SUPABASE_URL),
+			'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify(env.VITE_SUPABASE_ANON_KEY)
+		}
+	};
 });
