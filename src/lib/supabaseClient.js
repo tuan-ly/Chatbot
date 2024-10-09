4932c5a75f8b32c980f57b513d8b3924e450866a
tuan-ly <89176508+tuan-ly@users.noreply.github.com>
2024-10-10 05:04:43 +0700

    import meta

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$lib/config';
-
+// import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$lib/config';
+export const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+export const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export const createSupabaseWithToken = (token) => {
